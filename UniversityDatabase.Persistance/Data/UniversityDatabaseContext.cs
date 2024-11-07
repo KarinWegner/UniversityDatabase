553c6107dc99e2a4865efd626ee6489ce79ddb7b
Karin Wegner <karinwegner00@gmail.com>
2024-11-07 15:58:31 +0100

    Set up student controller and database

---
+++ UniversityDatabase.Persistance/Data/UniversityDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UniversityDatabase.Core.Entities;
+
+namespace UniversityDatabase.Persistance.Data
+{
+    public class UniversityDatabaseContext : DbContext
+    {
+        public UniversityDatabaseContext(DbContextOptions<UniversityDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; } = default!;
+    }
+}
--- UniversityDatabase/Program.cs
+++ UniversityDatabase/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UniversityDatabase.Persistance.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<UniversityDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityDatabaseContext") ?? throw new InvalidOperationException("Connection string 'UniversityDatabaseContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

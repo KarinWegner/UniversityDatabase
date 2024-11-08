a199d8e848d636abfe64ad0e51006526423f9968
Karin Wegner <karinwegner00@gmail.com>
2024-11-08 10:17:51 +0100

    Added enrollment

--- UniversityDatabase.Core/Entities/Enrollment.cs
+++ UniversityDatabase.Core/Entities/Enrollment.cs
@@ -8,14 +8,18 @@ namespace UniversityDatabase.Core.Entities
 {
     public  class Enrollment
     {
-        public int CourseId { get; set; }
         public int Grade {  get; set; }
 
         //Convention 1 och 3 
        // public Student Student { get; set; }
      
         //Convention 4 Add foreign key.
+
+        //Foreign Key
+        public int CourseId { get; set; }
         public int StudentId { get; set; }
+        //Navigational property
+        public Course Course { get; set; }
         public Student Student { get; set; }
     }
 }
